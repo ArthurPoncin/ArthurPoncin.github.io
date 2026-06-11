9bfb37a42f6b622ef8d0780e2c33169280b284f8
Arthur <poncin.arthur@proton.me>
2026-06-11 14:33:33 +0200

    Ajoute le support de soutenance MSPR3 (TPRE601)

---
+++ mspr3-soutenance/assets/slidev/default-4nbZOlAg.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-DKBIPrJq.js";import{_ as r}from"../index-DNkmaNQD.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
